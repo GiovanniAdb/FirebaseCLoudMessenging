8e8d57f58b019111c0e01ec71db3fe175b637cac
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2023-05-01 22:36:53 -0600

    Se agrego el metodo onMessageReceived(), para lograr mostrar la notificacion

--- app/src/main/java/com/example/firebasecloudmessenging/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/firebasecloudmessenging/MyFirebaseMessagingService.kt
@@ -1,4 +1,59 @@
 package com.example.firebasecloudmessenging
 
-class MyFirebaseMessagingService {
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        // Obtén el título y el cuerpo del mensaje
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+
+        // Crea una notificación
+        val notificationBuilder = NotificationCompat.Builder(this, "default")
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // Muestra la notificación
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 }

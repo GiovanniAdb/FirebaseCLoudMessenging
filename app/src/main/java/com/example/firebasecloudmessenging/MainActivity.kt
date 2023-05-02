ecce0f43b934f7c1bd227399b330b81ff3036b22
GiovanniAdb <78819879+GiovanniAdb@users.noreply.github.com>
2023-05-01 22:37:42 -0600

    Obténer el token de registro y mostrarlo en el logcat

--- app/src/main/java/com/example/firebasecloudmessenging/MainActivity.kt
+++ app/src/main/java/com/example/firebasecloudmessenging/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.firebasecloudmessenging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val token = task.result
+                Log.d("FCM", "Token: $token")
+            } else {
+                Log.w("FCM", "Error al obtener el token", task.exception)
+            }
+        }
     }
 }
